8d2be2983fc329e28cbdba43e8d232ceca323e1c
Jérémie Regnault <jeremie.regnault@capgemini.com>
2022-03-31 14:03:17 +0200

    Add Constructor to ApplicationDbContext

--- WishList/Data/ApplicationDbContext.cs
+++ WishList/Data/ApplicationDbContext.cs
@@ -4,5 +4,9 @@ namespace WishList.Data
 {
     public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
     }
 }
